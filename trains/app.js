52c86dce5cb39f43c6ed0455b0d1bea71f18d902
SAI KRISHNA S B <103356323+saigeek@users.noreply.github.com>
2023-09-03 11:59:58 +0530

    error

---
+++ trains/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios"
+
+const app = express();
+const port = 3000;
+
+//I was not able to register into the api , even though my raw file to register was correct , I received {
+    //"message": "accessCode field is invalid"} I was not able to figure it out
+
